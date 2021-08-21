b83270efef52a3a84d85fed8eacb958ebc148ef9
Sivan Kaplan <sivanagar@gmail.com>
2021-08-20 20:31:15 -0700

    display question after click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 var startBtn = document.querySelector("#start"); 
 var timerEl = document.getElementById('time');
+var mainEl = document.getElementById('main')
+var titleEl = document.querySelector("h1");
+console.log(mainEl);
+
+var score = 75;
 
 function startQuiz () {
     //start timer
     countdown();
+    //hide the start message
     //show question
+    showQuestion();
 }
 
 function countdown() {
-    var timeLeft = 75;
     var timeInterval = setInterval(function() {
-    if (timeLeft >0){
-        timerEl.textContent= timeLeft;
-        timeLeft --;
+    if (score >0 ){
+        timerEl.textContent= score;
+        score --;
     } else {
         timerEl.textContent= 0;
         clearInterval(timeInterval);
@@ -20,6 +26,27 @@ function countdown() {
     },1000);
 }
 
-console.log(startBtn);
+function showQuestion(){
+    titleEl.textContent ="A QUESTION";
+    //remove first page content
+    var subTitle = document.querySelector("h2");
+    subTitle.remove();
+    startBtn.remove();
+    mainEl.className ="main-question";
+    answers = ["answer1", "answer2", "answer3", "answer4"];
+    
+    for(var i=0 ; i<4 ; i++){
+        var answer1 = document.createElement("button");
+        answer1.className = "answer"
+        answer1.textContent = (i+1)+". " +answers[i];
+        mainEl.appendChild(answer1);
+}
+
+
+
+}
+
 //add event listener to button click and call function to start
 startBtn.addEventListener("click", startQuiz );
+
+
