fc119f79d02d57eb981bf8b586fc6ca449a70e4f
Sivan Kaplan <sivanagar@gmail.com>
2021-08-23 19:27:39 -0700

    Quiz Results and stop timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ var startBtn = document.querySelector("#start");
 var timerEl = document.getElementById("time");
 var mainEl = document.getElementById("main");
 var titleEl = document.querySelector("h1");
-console.log(mainEl);
+var answerOptions =document.createElement("div");
+var answerDisplay = document.createElement("span");
+answerDisplay.className = "result";
+answerDisplay.textContent = "";
+mainEl.appendChild(answerDisplay);  
+
 
 // list of all questions, choices, and answers
 var questions = [
@@ -50,38 +55,31 @@ var clickHandler = function(event) {
   if (targetEl.matches("#start")){
     startQuiz();
   } else if (targetEl.matches(".answer")){
-    console.log(targetEl);
     //check if the answer correct and show
     var answerId = targetEl.getAttribute("data-answer-id");
     checkAnswer(answerId);
-
   }
-
 };
 
 function checkAnswer(id) {
-  console.log(id);
-  console.log()
+  
   if (questions[questionCount].answer==questions[questionCount].choices[id]) {
-    //correct answer
-    console.log("correct");
-    //display correct answer div
+    answerDisplay.textContent = "Correct!";   //display correct answer div
   } else {
-    //wrong answer
+    //wrong answer 
     score -= 10;
-    //display wrong answer div
-    console.log("wrong");
+    answerDisplay.textContent = "Wrong!"; //display wrong answer div
   }
+  mainEl.appendChild(answerDisplay);
+  
   //call next question
-  if (questionCount == questions.length - 1) {
-    endQuiz();
-  } else {
-    questionCount ++;
+  questionCount ++;
+  if (questionCount !== questions.length) {
     for (var i = 0; i < 4; i++) { //remove old answers
-    var oldAnswers = document.querySelector(".answer");
-      oldAnswers.remove();
-    }
-  showQuestion(questionCount); //call for next question
+      var oldAnswers = document.querySelector(".answer");
+        oldAnswers.remove();
+      }
+    showQuestion(questionCount); //call for next question
   }
 }
 
@@ -92,56 +90,44 @@ function startQuiz() {
   subTitle.remove();
   startBtn.remove();
   //show question from array 
+  mainEl.className = "main-question"; 
+  answerOptions.className = "main-question";
+  mainEl.appendChild(answerOptions);
   showQuestion(questionCount);
-  //wait for click and
-
-  //check if correct
-
-  //if incorrect
-  // score -= 10;
-
-  //show next question
-
-  // remove q and a and endQuiz();
 }
 
 function countdown() {
   var timeInterval = setInterval(function () {
-    if (score > 0) {
+    if (score == 0 || questionCount == questions.length ) {
+      // stop timer and go to end of quiz
+      clearInterval(timeInterval);
+      timerEl.textContent = score;
+      endQuiz();
+    } else {
       timerEl.textContent = score;
       score--;
-    } else {
-      timerEl.textContent = 0;
-      // should stop questions and go to end of quiz
-      endQuiz();
-      clearInterval(timeInterval);
     }
   }, 1000);
 }
 
 function showQuestion(questionID) {
-  //should get  index so it can be called for each question in array of questions
+  //get  index so it can be called for each question in array of questions
   titleEl.textContent = questions[questionID].title;
-  mainEl.className = "main-question";
-  
-  
   //add answers 
   for (var i = 0; i < 4; i++) {
-
     var answer1 = document.createElement("button");
     answer1.className = "answer";
     answer1.setAttribute("data-answer-id" , i);
     answer1.textContent = ((i+1) + ". " + questions[questionID].choices[i]);
-    mainEl.appendChild(answer1);
+    answerOptions.appendChild(answer1);
   }
 }
 
-//write endQuiz function
 function endQuiz() {
-  //stop timer
   titleEl.textContent = "All done!";
-  //remove answers from screen
-  var showScore = document.createElement("h2");
+  answerOptions.remove(); //remove answers from screen
+  answerDisplay.remove(); //remove result display from screen
+  var showScore = document.createElement("h2"); 
   showScore.textContent = "Your final score is " + score;
   mainEl.appendChild(showScore);
   //add form element
